cdb46b2e01b50c2ba5f5d9b3d23636a373caea6a
GoSSy4691 <vitea1994@gmail.com>
2022-07-15 12:50:50 +0300

    style: main table

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import arrow from "./img/arrow.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Тестовое задание web-программист (React.js)</h2>
+        <h4>Таблица в формате Single Page Application</h4>
       </header>
+      <div className="page-box">
+        <img src={arrow} alt="previous" style={{ transform: "scale(-1, 1)" }} />
+        <img src={arrow} alt="next" />
+      </div>
+      <body>
+        <table id="table">
+          <tr>
+            <th>Дата</th>
+            <th>Название</th>
+            <th>Количество</th>
+            <th>Расстояние</th>
+          </tr>
+          <tr>
+            <td>07.12.22</td>
+            <td>ARYDELM</td>
+            <td>122</td>
+            <td>240</td>
+          </tr>
+        </table>
+      </body>
     </div>
   );
 }
